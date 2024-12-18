be57030a9efc5427b086c6a186e56de2da3b233e
Gaillard05 <maylisgaillard@hotmail.fr>
2024-12-18 13:53:38 +0100

    commenter une partie du code pour voir si les test fonctionnes correctement

--- math_utils.py
+++ math_utils.py
@@ -5,6 +5,6 @@ def calculate_average(numbers):
     :return: Moyenne des nombres (float).
     :raises ValueError: Si la liste est vide.
     """
-    if not numbers:
-        raise ValueError("La liste ne peut pas être vide.")
-    return sum(numbers) / len(numbers)
+    # if not numbers:
+    #     raise ValueError("La liste ne peut pas être vide.")
+    # return sum(numbers) / len(numbers)
